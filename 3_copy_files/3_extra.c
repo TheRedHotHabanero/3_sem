edbdb0235b28d2d5a0fea0251481ddc8b1bbcc75
theredhothabanero <lkrn10001@gmail.com>
2021-11-11 18:25:29 +0300

    correct now

--- 3_copy_files/3_extra.c
+++ 3_copy_files/3_extra.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
 #define MEMBLOCK 4096
 
 ssize_t p_write_all(int fd, const void *buf, size_t count, long int counter)
@@ -31,7 +32,7 @@ size_t copy_all(const int fd_1, const int fd_2)
   while(copy_var > 0)
   {
     void* buf = calloc(MEMBLOCK, sizeof(char));
-    copy_var = read(fd_1, buf, MEMBLOCK);
+    copy_var = pread(fd_1, buf, MEMBLOCK, counter);
     if(copy_var < 0)
     {
       perror("Failed to read file");
@@ -53,21 +54,21 @@ size_t copy_all(const int fd_1, const int fd_2)
 
 size_t create_symlink(const char* path, const char* name)
 {
-  char* buf = (char*)calloc(_PC_PATH_MAX, sizeof(char));
+  char* buf = (char*)calloc(PATH_MAX, sizeof(char));
+  size_t res = 0;
 
-  if (readlink(path, buf, _PC_PATH_MAX) == -1)
+  if (readlink(path, buf, PATH_MAX) < 0)
   {
     fprintf(stderr, "Failed while reading link\n");
-    free(buf);
-    return 10;
+    res = 10;
   }
-  if (symlink(buf, name) == -1)
+  if (symlink(buf, name) < 0)
   {
     fprintf(stderr, "Failed to create the linl\n");
-    free(buf);
-    return 11;
+    res = 11;
   }
-  return 0;
+  free(buf);
+    return res;
 }
 
 int main(int argc, char *argv[])
@@ -87,11 +88,53 @@ int main(int argc, char *argv[])
   }
 
  // macros from man fstat
-  switch (sb.st_mode & S_IFMT)
+  switch(sb.st_mode & S_IFMT)
   {
+    case S_IFREG:
+    {
+      int fd_1 = open(argv[1], O_RDONLY);
+      if (fd_1 < 0)
+      {
+        fprintf(stderr, "Failed to open %s\n", argv[1]);
+        return 3;
+      }
+
+      int fd_2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      if (fd_2 < 0)
+      {
+        fprintf(stderr, "Failed to open %s for writing\n", argv[2]);
+        return 4;
+      }
+
+      copy_all(fd_1, fd_2);
+
+      if(close(fd_1) < 0)
+      {
+        fprintf(stderr, "Failure while closing %s\n", argv[1]);
+        return 5;
+      }
+
+      if(close(fd_2) < 0)
+      {
+        fprintf(stderr, "Failure while closing %s\n", argv[2]);
+        return 6;
+      }
+    break;
+    }
+    case S_IFIFO:
+      if(mkfifo(argv[2], sb.st_mode) < 0)
+      {
+        fprintf(stderr, "Failed to create %s FIFO file\n", argv[2]);
+        return 13;
+      }
+      break;
+
+    case S_IFLNK:
+      create_symlink(argv[1], argv[2]);
+      break;
     case S_IFBLK:
     case S_IFCHR:
-      if (mknod(argv[2], sb.st_mode, sb.st_dev) == -1)
+      if(mknod(argv[2], sb.st_mode, sb.st_rdev) < 0)
       {
         fprintf(stderr, "Failed to create new character device");
         return 12;
@@ -99,53 +142,15 @@ int main(int argc, char *argv[])
       break;
     case S_IFDIR:
       printf("directory\n");
+      return 14;
       break;
-    case S_IFIFO:
-      if (mkfifo(argv[2], sb.st_mode) == -1)
-      {
-        fprintf(stderr, "Failed to create %s FIFO file\n", argv[2]);
-        return 13;
-      }
-      break;
-    case S_IFLNK:
-      create_symlink(argv[1], argv[2]);
-      break;
-    case S_IFREG:
-    {
-      int fd_1 = open(argv[1], O_RDONLY);
-      if (fd_1 == -1)
-      {
-        perror("Failed to open copied");
-        return 3;
-      }
-
-      int fd_2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-      if (fd_2 == -1)
-      {
-        perror("Failed to open file for writing");
-        return 4;
-      }
-
-      copy_all(fd_1, fd_2);
-
-      if (close(fd_1) < 0)
-      {
-        perror("Failure during close first");
-        return 5;
-      }
-
-      if (close(fd_2) < 0)
-      {
-        perror("Failure during close second");
-        return 6;
-      }
-    }
     case S_IFSOCK:
       printf("socket\n");
+      return 15;
       break;
     default:
-      fprintf(stderr, "File %s is not from available macros list", argv[1]);
-      break;
+      fprintf(stderr, "File %s is not type of 'Block device', 'Character device', 'Regular', 'FIFO' or 'Symlink'\n", argv[1]); 
+      return 16;
   }
   return 0;
 }
