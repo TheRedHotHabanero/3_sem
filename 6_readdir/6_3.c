b9a4799cbc4ac9ccec162c7e66fcad1bc2d5ab79
theredhothabanero <lkrn10001@gmail.com>
2021-12-12 21:34:03 +0300

    fixed version

--- 6_readdir/6_3.c
+++ 6_readdir/6_3.c
@@ -1,31 +1,15 @@
 #define _GNU_SOURCE
 #define BUF_SIZE 65536
+#define MEMBLOCK 4096
 
 #include <assert.h>
 #include <dirent.h>
-#include <errno.h>
 #include <fcntl.h>
-#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
-
-#define DIR_MODE 0777
-#define MEMBLOCK 4096
-#define NUMBER_OF_SIGNALS 32
-
-const int __signals__[NUMBER_OF_SIGNALS] =
-{
-  SIGABRT,   SIGALRM, SIGBUS,  SIGCHLD, SIGCLD,    SIGCONT, SIGFPE,
-  SIGHUP,    SIGILL,  SIGINT,  SIGIO,   SIGIOT,    SIGPIPE, SIGPOLL,
-  SIGPROF,   SIGPWR,  SIGQUIT, SIGSEGV, SIGSTKFLT, SIGTSTP, SIGSYS,
-  SIGTERM,   SIGTRAP, SIGTTIN, SIGTTOU, SIGURG,    SIGUSR1, SIGUSR2,
-  SIGVTALRM, SIGXCPU, SIGXFSZ, SIGWINCH
-};
 
 ssize_t write_all(int fd, const void *buf, size_t count)
 {
@@ -168,161 +152,13 @@ char mode_char(unsigned mode)
   return '?';
 }
 
-ssize_t copy_dir(DIR *old_dir, DIR *new_dir)
-{
-  ssize_t result = 0;
-
-  struct dirent *entry;
-  struct stat sb;
-
-  // obtaining file descriptors of our old and new directories
-  int old_dir_fd = dirfd(old_dir);
-  int new_dir_fd = dirfd(new_dir);
-
-  while ((entry = readdir(old_dir)) != NULL)
-  {
-    char entry_type = '?';
-    if (fstatat(old_dir_fd, entry->d_name, &sb, AT_SYMLINK_NOFOLLOW) < 0)
-    {
-      perror("fstatat");
-    }
-    else
-    {
-      entry_type = mode_char(sb.st_mode);
-    }
-
-    if (strcmp(entry->d_name, ".") && strcmp(entry->d_name, ".."))
-    {
-      switch (entry_type)
-      {
-      // regular files
-      case '-': {
-        int old_file_fd = openat(old_dir_fd, entry->d_name, O_RDONLY);
-        int new_file_fd = openat(new_dir_fd, entry->d_name,
-                                 O_WRONLY | O_CREAT | O_TRUNC, 0777);
-
-        if (new_file_fd < 0)
-        {
-          perror("openat");
-          result = -1;
-        }
-
-        if (copy_all(old_file_fd, new_file_fd, &sb) < 0)
-        {
-          fprintf(stderr, "Failure while copying file <%s>\n", entry->d_name);
-          result = -1;
-        }
-
-        close(old_file_fd);
-        close(new_file_fd);
-        break;
-      }
-
-      // symlinks
-      case 'l':
-      {
-        if (crt_linkat(old_dir_fd, new_dir_fd, entry->d_name, entry->d_name) <
-                0 &&
-            errno != EEXIST)
-        {
-          fprintf(stderr, "Failure while copying link <%s>\n", entry->d_name);
-          result = -1;
-        }
-        break;
-      }
-
-      // block devices and character devices can be created by the same function
-      // so they merged
-      case 'b':
-      case 'c':
-      {
-        if (mknodat(new_dir_fd, entry->d_name, sb.st_mode, sb.st_rdev) < 0 &&
-            errno != EEXIST)
-        {
-          perror("mknodat");
-          fprintf(stderr, "Failed to copy block/character device <%s>\n", entry->d_name);
-          result = -1;
-        }
-
-      } break;
-
-      // fifo/pipe
-      case 'p':
-      {
-        if (mkfifoat(new_dir_fd, entry->d_name, sb.st_mode) < 0 &&
-            errno != EEXIST)
-        {
-          perror("mkfifoat");
-          result = -1;
-        }
-
-      } break;
-
-      // directiories
-      case 'd':
-      {
-        int old_nest_dirfd =
-            openat(old_dir_fd, entry->d_name, O_RDONLY | O_DIRECTORY);
-        if (old_nest_dirfd < 0)
-        {
-          perror("openat");
-          result = -1;
-        }
-        DIR *old_nest_dir = fdopendir(old_nest_dirfd);
-
-        // if directory with name <entry->d_name> already exists, then it will
-        // be just opened. Otherwise, it will be created by mkdirat()
-        if (mkdirat(new_dir_fd, entry->d_name, DIR_MODE) < 0 && errno != EEXIST)
-        {
-          perror("mkdirat");
-          result = -1;
-        }
-
-        int new_nest_dirfd =
-            openat(new_dir_fd, entry->d_name, O_RDONLY | O_DIRECTORY);
-        if (new_nest_dirfd < 0)
-        {
-          perror("openat");
-          result = -1;
-        }
-        DIR *new_nest_dir = fdopendir(new_nest_dirfd);
-
-        if (copy_dir(old_nest_dir, new_nest_dir) < 0)
-        {
-          fprintf(stderr, "Failure while copying directory <%s>\n",
-                  entry->d_name);
-          result = -1;
-        }
-
-        if (closedir(old_nest_dir) < 0)
-        {
-          perror("closedir");
-          result = -1;
-        }
-
-        if (closedir(new_nest_dir) < 0)
-        {
-          perror("closedir");
-          result = -1;
-        }
-      } break;
-
-      default:
-        printf("Unknown file type\n");
-      }
-    }
-  }
-
-  return result;
-}
-
 struct linux_dirent64
 {
-  ino64_t d_ino;           /* 64-bit inode number */
-  off_t d_off;             /* 64-bit offset to next structure */
-  unsigned short d_reclen; /* Size of this dirent */
-  unsigned char d_type;    /* File type */
-  char d_name[];           /* Filename (null-terminated) */
+  ino64_t d_ino;           // 64-bit inode number
+  off_t d_off;             // 64-bit offset to next structure
+  unsigned short d_reclen; // Size of this dirent
+  unsigned char d_type;    // File type
+  char d_name[];           // Filename (null-terminated)
 };
 
 int main(int argc, char *argv[])
