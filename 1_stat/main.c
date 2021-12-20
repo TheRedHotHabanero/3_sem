f46fda766139804d3e31d0a26ecc8850356a9458
theredhothabanero <lkrn10001@gmail.com>
2021-12-20 14:52:43 +0300

    fixed types & cahnged bufs function & comments

--- 1_stat/main.c
+++ 1_stat/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>      //lstat
-#include <sys/sysmacros.h> //fstat
-#include <sys/types.h>     //stat
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <sys/types.h>
 #include <time.h>
-#define BITS 1023
+//#define BITS 1023 // 2_ or 8_ perms sys/stat.h
+// gcc -Werror -Wall -Wextra -Wnarrowing -Wconversion -Wwrite-strings -Wcast-qual -Wundef -Wstrict-prototypes -Wbad-function-cast -Wlogical-op -Wreturn-type -g -fsanitize=address,undefined main.c -o a.out
+// gcc -Werror -Wall -Wextra -Wnarrowing -Wconversion -Wwrite-strings -Wcast-qual -Wundef -Wstrict-prototypes -Wbad-function-cast -Wlogical-op -Wreturn-type -g main.c -o main.exe && valgrind ./main.exe main.c
+// clang -Werror -Wall -Wextra -Wnarrowing -Wconversion -Wwrite-strings -Wcast-qual -Wundef -Wstrict-prototypes -Wbad-function-cast -Wreturn-type -g -fsanitize=memory main.c -o main.exe
+// https://pubs.opengroup.org/onlinepubs/009695399/basedefs/sys/stat.h.html
+
+#ifndef S_BLKSIZE
+#include <sys/param.h>
+#ifdef DEV_BSIZE
+#define S_BLKSIZE DEV_BSIZE
+#else
+#error "Couldn't determine block size for this platfrom"
+#endif
+#endif
 
 int nano_time(char* out_buf, size_t buf_len, const struct timespec* file_time)
 {
@@ -19,17 +33,21 @@ int nano_time(char* out_buf, size_t buf_len, const struct timespec* file_time)
   return snprintf(out_buf, buf_len, "%s.%09ld %s", current_time, file_time->tv_nsec, add_time);
 }
 
-void bufs(char* buf, const unsigned mode)
+void bufs(char* buf, const mode_t mode)
 {
-  buf[0] = mode & S_IRUSR ? 'r' : '-';
-  buf[1] = mode & S_IWUSR ? 'w' : '-';
-  buf[2] = mode & S_IXUSR ? 'x' : '-';
-  buf[3] = mode & S_IRGRP ? 'r' : '-';
-  buf[4] = mode & S_IWGRP ? 'w' : '-';
-  buf[5] = mode & S_IXGRP ? 'x' : '-';
-  buf[6] = mode & S_IROTH ? 'r' : '-';
-  buf[7] = mode & S_IWOTH ? 'w' : '-';
-  buf[8] = mode & S_IXOTH ? 'x' : '-';
+  *buf++ = mode & S_IRUSR ? 'r' : '-';
+  *buf++ = mode & S_IWUSR ? 'w' : '-';
+  *buf++ = mode & S_IXUSR ? 'x' : '-';
+  *buf++ = mode & S_IRGRP ? 'r' : '-';
+  *buf++ = mode & S_IWGRP ? 'w' : '-';
+  *buf++ = mode & S_IXGRP ? 'x' : '-';
+  *buf++ = mode & S_IROTH ? 'r' : '-';
+  *buf++ = mode & S_IWOTH ? 'w' : '-';
+  *buf++ = mode & S_IXOTH ? 'x' : '-';
+  *buf = '\0';
+  // терм 0
+  // заменить на цикл из трех строчек + 3 бита сетюид, сетгид и чет еще забыла
+  // cppcheck
 }
 
 int main(int argc, char *argv[])
@@ -51,7 +69,7 @@ int main(int argc, char *argv[])
   }
 
   printf("File name:                %s\n", argv[1]);
-  printf("ID of containing device:  [%lx,%lx]\n", (long) major(sb.st_dev), (long) minor(sb.st_dev));
+  printf("ID of containing device:  [%xh,%xh]\n", major(sb.st_dev), minor(sb.st_dev));
   printf("File type:                ");
 
   switch (sb.st_mode & S_IFMT)
@@ -66,15 +84,15 @@ int main(int argc, char *argv[])
     default:       printf("unknown?\n");                break;
   }
 
-  printf("I-node number:            %ld\n", (long) sb.st_ino);
-  printf("Mode:                     %lo (octal)\n", (unsigned long) sb.st_mode);
-  printf("Link count:               %ld\n", (long) sb.st_nlink);
+  printf("I-node number:            %ju\n", (long) sb.st_ino);
+  printf("Mode:                     %o (octal)\n", sb.st_mode);
+  printf("Link count:               %ju\n", (uintmax_t) sb.st_nlink);
   bufs(perm_str, sb.st_mode);
-  printf("Access:                   %o/%s\n", sb.st_mode & BITS, perm_str);
-  printf("Ownership:                UID=%ld   GID=%ld\n", (long) sb.st_uid, (long) sb.st_gid);
-  printf("Preferred I/O block size: %ld bytes\n", (long) sb.st_blksize);
-  printf("File size:                %lld bytes\n", (long long) sb.st_size);
-  printf("Blocks allocated:         %lld\n", (long long) sb.st_blocks);
+  printf("Access:                   %04o/%s\n", sb.st_mode & ALLPERMS, perm_str);
+  printf("Ownership:                UID=%ju   GID=%ju\n", (uintmax_t) sb.st_uid, (uintmax_t) sb.st_gid);
+  printf("Preferred I/O block size: %ju bytes\n", (uintmax_t) sb.st_blksize);
+  printf("File size:                %ju bytes\n", (uintmax_t) sb.st_size);
+  printf("Blocks allocated:         %ju bytes)\n", (uintmax_t) sb.st_blocks);
 
 	//------------------------------------------------------special time format
   nano_time(string, sizeof(string), &sb.st_ctim);
