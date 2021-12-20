d99d5c54db8221b63c547eb9eee181112296f036
theredhothabanero <lkrn10001@gmail.com>
2021-12-20 14:36:49 +0300

    new sig_handler function, main fixed

--- 15_signals/15_1.c
+++ 15_signals/15_1.c
@@ -3,35 +3,25 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <errno.h>
 
 volatile int g_last_signal;
-volatile siginfo_t *g_from_who;
 
-const int signls[_NSIG] = {
-    SIGABRT,   SIGALRM, SIGBUS,  SIGCHLD, SIGCLD,    SIGCONT, SIGFPE,
-    SIGHUP,    SIGILL,  SIGINT,  SIGIO,   SIGIOT,    SIGPIPE, SIGPOLL,
-    SIGPROF,   SIGPWR,  SIGQUIT, SIGSEGV, SIGSTKFLT, SIGTSTP, SIGSYS,
-    SIGTERM,   SIGTRAP, SIGTTIN, SIGTTOU, SIGURG,    SIGUSR1, SIGUSR2,
-    SIGVTALRM, SIGXCPU, SIGXFSZ, SIGWINCH};
-
-void sig_handler(int signum, siginfo_t *info, void *ucontext) {
-  (void)ucontext;
+void sig_handler(int signum)
+{
   g_last_signal = signum;
-  g_from_who = info;
 }
 
 int main() {
-  int result = 0;
+  struct sigaction action = {
+    .sa_flags = SA_RESTART,
+    .sa_handler = sig_handler,
+  };
+  sigfillset(&action.sa_mask);
 
-  struct sigaction recieved = {};
-
-  recieved.sa_flags = SA_SIGINFO;
-  recieved.sa_sigaction = sig_handler;
-
-  for (int i = 0; i < _NSIG; i++) {
-    if (sigaction(signls[i], &recieved, NULL) < 0) {
-      perror("sigaction");
-      return -1;
+  for (int i = 0; i <= _NSIG; i++) {
+    if (sigaction(i, &action, NULL) < 0) {
+      fprintf(stderr, "...%d: %s\n", i, strerror(errno));
     }
   }
 
